77eedeaf81ee897ad133c42c45c712c52654b549
jacyhowe <jacyhowe@hotmail.com>
2019-02-18 20:31:09 -0700

    built sign in and sign up components and routes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
+import history from './history';
+import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
 import './style/main.scss';
 
 import Layout from './components/layout';
@@ -17,11 +19,15 @@ import Layout from './components/layout';
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}> 
         <Layout>
+          <Switch>
+            <Route path='/' exact component={Signin}/>
+            <Route path='/signup' exact component={Signup}/>
+          </Switch>
     
         </Layout>
-      </BrowserRouter>
+      </Router>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
---
+++ src/components/auth/signin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    render() {
+        return (
+            <div className='sign-in'>Sign in</div>
+        )
+    }
+}
+
+export default Signin;

af04d8df89f3825ccaea12fcee674fb96696bd39
jacyhowe <jacyhowe@hotmail.com>
2019-03-05 18:55:22 -0700

    built base structure for account components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -14,6 +14,7 @@ import Signup from './components/auth/signup';
 import './style/main.scss';
 
 import Layout from './components/layout';
+import Account from './components/account/account';
 
 
 function main() {
@@ -25,6 +26,8 @@ function main() {
             <Route path='/' exact component={Signin}/>
             <Route path='/signin' exact component={Signin}/>
             <Route path='/signup' exact component={Signup}/>
+            
+            <Route path='/account' exact component={Account}/>
           </Switch>    
         </Layout>
       </Router>
---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AccountInformation extends Component {
+    render() {
+        return (
+            <div>
+                account history                
+            </div>
+        )
+    }
+}
+export default AccountInformation;

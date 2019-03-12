75b5e4218724db5e4d0c9da47934fe757d28edb4
jacyhowe <jacyhowe@hotmail.com>
2019-03-11 18:24:51 -0600

    setup purchase history child components and corresponding styles

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -28,13 +28,13 @@ class Account extends Component {
             {
                 _id: 0,
                 title: 'Purchase History',
-                active: false,
+                active: true,
                 component: <PurchaseHistory/>
             },
             {
                 _id: 1,
                 title: 'Account Information',
-                active: true,
+                active: false,
                 component: <AccountInformation/>
             }
         ]
@@ -57,8 +57,8 @@ class Account extends Component {
 
     render() {
         return (
-            <div>
-           { this.renderContent() }
+            <div className='account'>
+                { this.renderContent() }
            </div>
         )
     }

da2c69204f1257061083accba9a7d9e2b0f3bce6
jacyhowe <jacyhowe@hotmail.com>
2019-03-18 19:48:02 -0600

    built account information grid and placed majority of fields on grid.

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -11,46 +11,71 @@ class AccountInformationForm extends Component {
         const { className, handleSubmit } = this.props;
         
         return (
-            <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
-                <Field className='sign-up-form__name'
+            <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+                <Field className='account-information-form__name'
                 type='name'
                 title='Name'
                 placeholder='Name'
                 name='name'
                 component={FormInput}/>
-                <Field className='sign-up-form__email'
+                <Field className='account-information-form__email'
                 type='email'
                 title='Email'
                 placeholder='Email'
                 name='email'
                 component={FormInput}/>
-                <Field className='sign-up-form__password'
+                <Field className='account-information-form__street-address'
+                type='address'
+                title='Street Address'
+                placeholder='Street Address'
+                name='address'
+                component={FormInput}/>
+                <Field className='account-information-form__city'
+                type='city'
+                title='City'
+                placeholder='City'
+                name='city'
+                component={FormInput}/>
+                <Field className='account-information-form__state'
+                type='state'
+                title='State '
+                placeholder='State'
+                name='state'
+                component={FormInput}/>
+                <Field className='account-information-form__zip-code'
+                type='zip code'
+                title='Zip Code'
+                placeholder='Zip Code'
+                name='zip code'
+                component={FormInput}/>
+
+
+                {/* <Field className='account-information-form__password'
                 type='password'
                 title='Password'
                 placeholder='Password'
                 name='password'
                 component={FormInput}/>
-                <Field className='sign-up-form__confirm'
+                <Field className='account-information-form__confirm'
                 type='password'
                 title='Confirm Password'
                 placeholder='Confirm Password'
                 name='confirm'
                 component={FormInput}/>
 
-                <div className='sign-up-form__line'></div>
-                <Field className='sign-up-form__login'
+                <Field className='account-information-form__login'
                 onClick={() => history.push('/account')}
                 type='submit'
                 title='Creat Account'
                 name='login'
                 component={FormButton}/>
-                <Field className='sign-up-form__back'
+                <Field className='account-information-form__back'
                 onClick={() => history.push('/signin')}
                 type='button'
                 title='Back'
                 name='back'
                 short={true}
-                component={FormButton}/>
+                component={FormButton}/> */}
                 
             </form>
         )

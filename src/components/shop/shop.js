c4c13b32685bc8ca270ea2b78681350da0b215a3
jacyhowe <jacyhowe@hotmail.com>
2019-04-05 19:34:25 -0600

    added built shop grids finihsed back of shop product component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -35,7 +35,7 @@ class Shop extends Component {
     render() {
         return (
             <div className='shop'>
-                <ShopSearchBar onSubmit={this.onSubmit} classNmame='shop__search-bar'/>
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
                 <div className='shop__products'>
                 {
                     this.props.filteredProducts.map(product => {

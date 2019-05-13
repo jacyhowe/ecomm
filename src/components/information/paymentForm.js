045688498f9b401cfe02bb33430e2f68cf1534e9
jacyhowe <jacyhowe@hotmail.com>
2019-05-13 15:35:14 -0600

    finished shopping form component and sub components

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formField';
-
 import history from '../../history';
 
 class PaymentForm extends Component {
